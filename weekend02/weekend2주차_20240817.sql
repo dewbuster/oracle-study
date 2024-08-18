6c7d278c79537fc1bfaea5aae2e1b8e548925808
dewbuster <chwon301@gmail.com>
2024-08-18 17:35:59 +0900

    weekend02-2

--- weekend02/weekend2주차_20240817.sql
+++ weekend02/weekend2주차_20240817.sql
@@ -13,6 +13,91 @@ WHERE c.car_id NOT IN (
 AND (daily_fee * 30) * (100-discount_rate) / 100 BETWEEN 500000 AND 1999999
 ORDER BY FEE DESC, c.car_type, car_id DESC;
 
+--5월 식품들의 총매출 조회하기
+SELECT p.PRODUCT_ID, p.PRODUCT_NAME, SUM(p.PRICE*o.AMOUNT) TOTAL_SALES
+FROM FOOD_PRODUCT p JOIN FOOD_ORDER o ON p.PRODUCT_ID = o.PRODUCT_ID
+WHERE TO_CHAR(PRODUCE_DATE, 'YYYY-MM') = '2022-05'
+GROUP BY p.PRODUCT_ID, p.PRODUCT_NAME
+ORDER BY TOTAL_SALES DESC, PRODUCT_ID ASC
+
+--주문량이 많은 아이스크림들 조회하기
+SELECT
+        FLAVOR
+  FROM (
+        SELECT 
+                A.FLAVOR,
+                SUM(A.TOTAL_ORDER + B.TOTAL_ORDER) AS TOTAL_ORDER
+          FROM FIRST_HALF A,JULY B
+         WHERE A.FLAVOR = B.FLAVOR         
+       GROUP BY A.FLAVOR
+       ORDER BY TOTAL_ORDER DESC
+       )
+ WHERE ROWNUM <= 3
+
+--조건에 맞는 도서와 저자 리스트 출력하기
+SELECT a.book_id, b.author_name,
+    TO_CHAR(a.published_date,'YYYY-MM-DD')
+FROM book a JOIN author b
+    ON a.author_id = b.author_id
+WHERE category = '경제'
+ORDER BY a.published_date ASC;
+
+--그룹별 조건에 맞는 식당 목록 출력하기
+select MEMBER_NAME,REVIEW_TEXT,to_char(REVIEW_DATE,'YYYY-mm-dd')as REVIEW_DATE
+from MEMBER_PROFILE a join REST_REVIEW b on a.MEMBER_ID = b.MEMBER_ID
+where b.MEMBER_ID in (select MEMBER_ID
+from REST_REVIEW
+group by MEMBER_ID
+having count(MEMBER_ID) = (select max(count(MEMBER_ID)) from REST_REVIEW group by member_id ))
+order by REVIEW_DATE,REVIEW_TEXT
+
+--없어진 기록 찾기
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_OUTS O 
+LEFT JOIN ANIMAL_INS I
+ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE I.ANIMAL_ID IS NULL
+ORDER BY o.ANIMAL_ID;
+
+--있었는데요 없었습니다
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS I
+JOIN ANIMAL_OUTS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE O.DATETIME < I.DATETIME
+ORDER BY I.DATETIME;
+
+--오랜 기간 보호한 동물(1)
+SELECT NAME, DATETIME
+FROM (
+        SELECT 
+        I.NAME
+       ,I.DATETIME
+       ,RANK() OVER (ORDER BY I.DATETIME) AS RANK
+        FROM ANIMAL_INS I 
+        LEFT JOIN ANIMAL_OUTS O
+        ON I.ANIMAL_ID = O.ANIMAL_ID
+        WHERE O.ANIMAL_ID IS NULL
+) R
+WHERE R.RANK <= 3
+ORDER BY R.DATETIME;
+
+--보호소에서 중성화한 동물
+SELECT b.ANIMAL_ID,b.ANIMAL_TYPE,b.NAME
+from ANIMAL_INS a 
+join ANIMAL_OUTS b 
+on a.ANIMAL_ID = b.ANIMAL_ID and a.SEX_UPON_INTAKE != b.SEX_UPON_OUTCOME
+order by b.ANIMAL_ID;
+
+--상품 별 오프라인 매출 구하기
+SELECT a.PRODUCT_CODE
+    , SUM(PRICE * SALES_AMOUNT) sales
+FROM PRODUCT a JOIN OFFLINE_SALE b
+    ON a.PRODUCT_ID = b.PRODUCT_ID
+GROUP BY a.PRODUCT_CODE
+ORDER BY sales DESC
+    , a.PRODUCT_CODE ASC;
+
 -- 상품을 구매한 회원 비율 구하기
 SELECT EXTRACT(year from sales_date) YEAR
 , EXTRACT(month from sales_date) MONTH
